70b8a1fcf02d83f6e766d05180e39ea5205dab78
TijanaTanaskovic <tijana.kg98@gmail.com>
2022-11-17 23:22:01 +0100

    importing csv file; spliting and parsing it; validating date;

--- atof_improved_assignment/Program.cs
+++ atof_improved_assignment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace atof_improved_assignment
 {
@@ -6,6 +7,43 @@ namespace atof_improved_assignment
     {
         static void Main(string[] args)
         {
+            String[] fajl = System.IO.File.ReadAllLines(@"C:\Users\Tijana\source\repos\atof_improved_assignment\atof_improved_assignment\tijana_csv_proba.csv");
+
+            /*List<String> datum = new List<String>();
+            List<String> rezultat = new List<String>();
+            List<String> komentar = new List<String>();*/
+
+            //umesto ovog gore cilj je da imam List<Uzorak> sa parametrima klase Uzorak
+            //kako bih mogla da im pristupam u grupi i da ne zagubim usput
+
+            List<Uzorak> listaUzorka = new List<Uzorak>();
+
+
+            for (int i = 1; i < fajl.Length; i++)
+            {
+                //Console.WriteLine(fajl[i]); //svaka linija
+
+                String[] fajlSplit = fajl[i].Split(",");
+
+                listaUzorka.Add(new Uzorak(fajlSplit[0], fajlSplit[1], fajlSplit[2]));
+
+                /*datum.Add(fajlSplit[0]);
+                rezultat.Add(fajlSplit[1]);
+                komentar.Add(fajlSplit[2]);*/
+
+            }
+
+            foreach (var clan in listaUzorka)
+            {
+                Console.WriteLine(clan.Komentar);
+            }
+
+
+            /*foreach (var a in datum)
+            {
+                Console.WriteLine(a);
+            }*/
+            //proba stampanja da vidim sta imam u listama tacno
 
         }
     }
---
+++ atof_improved_assignment/Uzorak.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace atof_improved_assignment
+{
+    class Uzorak
+    {
+        private String komentar;
+        private DateTime datum;
+        private float rezultat;
+        private bool validan = true; //za validaciju podataka (datum, rezultat)
+
+        public String Komentar
+        {
+            get { return komentar; }
+            set { komentar = value; }
+        }
+        public DateTime Datum
+        {
+            get { return datum; }
+            set { datum = value; }
+        }
+
+        public float Rezultat
+        {
+            get { return rezultat; }
+            set { rezultat = value; }
+        }
+
+        public bool Validan
+        {
+            get { return validan; }
+            set { validan = value; }
+        }
+
+        public Uzorak(string datum, string rezultat, string komentar)
+        {
+            string[] dozvoljeniFormati = { "dd/MM/yyyy", "dd.MM.yyyy", "dd.MM.yyyy." };
+            this.komentar = komentar;
+            this.rezultat = 1;
+
+            bool validanDatum = validirajDatum(datum);
+            if (validanDatum)
+            {
+                DateTime dateValue;
+
+                DateTime.TryParseExact(datum, dozvoljeniFormati,
+                         new CultureInfo("en-US"),
+                         DateTimeStyles.None,
+                         out dateValue);
+
+                this.datum = dateValue;
+
+            }
+            else
+            {
+                this.validan = false;
+            }
+        }
+
+        public bool validirajRezultat(string rezultat)
+        {
+            return false;
+            //uradi
+        }
+
+        private bool validirajDatum(string date)
+        {
+            string[] formats = { "dd/MM/yyyy", "dd.MM.yyyy", "dd.MM.yyyy." };
+            DateTime dateValue;
+            if (DateTime.TryParseExact(date, formats,
+                      new CultureInfo("en-US"),
+                      DateTimeStyles.None,
+                      out dateValue))
+            {
+                Console.WriteLine("Converted '{0}' to {1}.", date, dateValue);
+                return true;
+            }
+
+            else
+            {
+                Console.WriteLine("Unable to convert '{0}' to a date.", date);
+                return false;
+            }
+
+
+        }
+    }
+}
